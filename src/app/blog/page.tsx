ca2656391b431851dbf8e11a93f2d8eb79b34c10
Darric Heng <darricheng@gmail.com>
2023-08-27 15:13:12 +0800

    test error handling

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,6 @@ import { fetchAllPosts } from "@/src/lib/utils/serverDataServices";
 
 export default async function Blog() {
   let postsData = await fetchAllPosts();
-  if (!postsData) postsData = [];
   return (
     <div>
       <h1>Blog</h1>
--- src/lib/utils/serverDataServices.ts
+++ src/lib/utils/serverDataServices.ts
@@ -2,9 +2,14 @@ const protocol = process.env.VERCEL_ENV !== "development" ? "https:" : "http:";
 const baseUrl = `${protocol}//${process.env.VERCEL_URL}`;
 
 export const fetchAllPosts = async () => {
-  const res = await fetch(`${baseUrl}/api/posts`, { cache: "no-store" });
-  const json = await res.json();
-  return json.data;
+  try {
+    const res = await fetch(`${baseUrl}/api/posts`, { cache: "no-store" });
+    const json = await res.json();
+    return json.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 export const fetchPost = async (id: number) => {
