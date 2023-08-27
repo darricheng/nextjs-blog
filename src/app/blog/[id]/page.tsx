f329156d418f0817cfe86fefc5488c941fa98313
Darric Heng <darricheng@gmail.com>
2023-08-27 16:16:03 +0800

    add dates to posts

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { fetchPost } from "@/src/lib/utils/serverDataServices";
 
 type PostProps = {
@@ -8,9 +9,17 @@ type PostProps = {
 
 export default async function Post({ params }: PostProps) {
   const postData = await fetchPost(params.id);
+  const createdTime = dayjs(postData.createdAt).format("D/M/YYYY - h:mma");
+  const lastModifiedTime = dayjs(postData.lastModifiedAt).format(
+    "D/M/YYYY - h:mma"
+  );
+  const dateStyle = "my-0 text-sm text-gray-500";
   return (
     <article>
       <h1>{postData.title}</h1>
+      <p className={dateStyle}>Created: {createdTime}</p>
+      <p className={dateStyle}>Last Edited: {lastModifiedTime}</p>
+      <hr />
       <p>{postData.content}</p>
     </article>
   );
